dae3ebf57d698a582498ebe21c32271cc3dc9379
Brandon <brandongoldney@gmail.com>
2025-09-29 12:48:37 -0400

    FIX: working again [skip ci]

--- app/view/assets/images/index.ts
+++ app/view/assets/images/index.ts
@@ -2,3 +2,4 @@ export { default as HomePageBackground } from "./home-page-background.png";
 export { default as businessAnalyst } from "./business-analyst.png";
 export { default as dataScientist } from "./data-scientist.png";
 export { default as productManager } from "./product-manager.png";
+export { default as GettingStartedBackgroundImage } from "./getting-started.png";
--- app/view/pages/index.ts
+++ app/view/pages/index.ts
@@ -1,6 +1,6 @@
 export { default as HomePage } from "./HomePage";
 export { default as MissingPage } from "./Missing";
-export { default as ComingSoon } from "./ComingSoon";
+export { default as ComingSoon } from "./comingSoon";
 export { default as ContactUs } from "./ContactUs";
 export { default as WatchDemo } from "./WatchDemo";
 export { default as Pricing } from "./pricing";
